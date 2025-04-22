0c1e485df8dcdc4249e5f2bf9163e2fa7435d6a9
sinla <sinla9302@naver.com>
2025-04-22 14:41:20 +0900

    boar_front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PostList from './pages/PostList';
+import PostDetail from './pages/PostDetail';
+import PostWrite from './pages/PostWrite';
+import PostEdit from './pages/PostEdit';
+import BoardPage from './pages/BoardPage'; // 🔥 인프런 스타일 게시판
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                {/* ✅ 메인 페이지를 BoardPage로 설정 */}
+                <Route path="/" element={<BoardPage />} />
+
+                {/* 기존 PostList는 별도 경로로 이동 */}
+                <Route path="/classic" element={<PostList />} />
+
+                <Route path="/posts/:id" element={<PostDetail />} />
+
+                {/* 게시글 상세/작성/수정 */}
+                <Route path="/write" element={<PostWrite />} />
+                <Route path="/posts/:id/edit" element={<PostEdit />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
